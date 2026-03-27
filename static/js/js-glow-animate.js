74aa287fd3311c17e1a1fda4ccb7940b0be9a82c
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2026-03-27 10:31:38 -0400

    Animation progress

--- static/js/js-glow-animate.js
+++ static/js/js-glow-animate.js
@@ -1,5 +1,4 @@
-const anchorId = 'js-glow-animate';
-const anchorElement = document.getElementById(anchorId);
+const anchorElement = document.getElementById('js-glow-animate');
 
 /*
     Adding class a.hover:glow is not necessary, since
@@ -26,7 +25,8 @@ Thus JavaScript is required to achieve this effect.
 
 */
 
-function revertAnimation() {
+function getComputedTextShadowValue(element) {
+    const computedStyles = getComputedStyle(element);
     /*
         [question]:
         Why does getPropertyValue exist?
@@ -34,20 +34,23 @@ function revertAnimation() {
         Maybe to offer a better interface that maps
         .. to CSS property identifiers rather than their JavaScript object property key representations.
     */
-    const computedAnchorStyles = getComputedStyle(anchorElement);
-    const currentTextShadowValue = computedAnchorStyles.getPropertyValue('text-shadow');
+    const currentTextShadowValue = computedStyles.getPropertyValue('text-shadow');
     
+    return currentTextShadowValue;
+}
+
+/*
+    We could use CSS to animate these keyframes,
+    enabling them to play upon hover by adding a .hover\:glow class once the hidden animation occurs,
+    and upon the first mouse hover.
+    
+    This would be a hassle to manage however, as we would need to first get the text shadow value from the
+    glow animation, and then ensure the glow animation is removed?
+*/
+function showTextShadow(element) {
     if (currentTextShadowValue == "") {
-        throw new Error(`\`text-shadow\` CSS property has no value for element with id ${anchorId}.`);
+        throw new Error(`\`text-shadow\` property not present in computed styles for element ${element}.`);
     }
-    
-    const currentTextShadowValueConstituents = currentTextShadowValue.split(' ');
-    const textShadowXOffset = currentTextShadowValueConstituents[0];
-    const textShadowYOffset = currentTextShadowValueConstituents[1];
-    
-    // Now that we have our info from the current glow animation state,
-    // .. we may safely remove it and start the dynamic revert animation.
-    anchorElement.classList.remove('hover:glow');
     
     /*
         Fulfills the format consumed by Element.animate():
@@ -58,7 +61,106 @@ function revertAnimation() {
             // A fallback for older browsers which don't support color deconstruction:
             textShadow: "0 0 0 var(--color-white);",
             // --color-off-white2 points to a deconstructed and darkend version of --color-white:
-            textShadow: `${textShadowXOffset} ${textShadowYOffset} 0 var(--color-off-white2)`,
+            // textShadow: `${textShadowXOffset} ${textShadowYOffset} 0 var(--color-off-white2)`,
+            textShadow: currentTextShadowValue,
+        },
+        
+        { textShadow: "0 0 0 transparent" },
+    ];
+    const timingObject = {
+        duration: 1000, // <- Milliseconds
+        iterations: 1,
+        fill: "forwards",
+    };
+    
+    const animation = anchorElement.animate(keyframeObject, timingObject);
+    
+    return animation;
+}
+
+/*
+ * Retrieves the current text shadow of an element,
+ * then pauses all animations on the element & animates from
+ * the retrieved text shadow values to a transparent shadow
+ * 
+ * Over-engineering at its finest :)
+ */
+function hideTextShadow(element) {
+    debugger;
+
+    const currentTextShadowValue = getComputedTextShadowValue(element);
+    
+    if (currentTextShadowValue == "") {
+        throw new Error(`\`text-shadow\` property not present in computed styles for element ${element}.`);
+    }
+    
+    // We need not parse the text-shadow value ourselves.
+    // We can just use it directly in the `from` animation keyframe data structure.
+    
+    // ----------------------
+    // Parse text-shadow value:
+    // [ text-shadow-string1, text-shadow-string2, ... ]
+    // const currentTextShadowsFromValue = currentTextShadowValue.split(',');
+    // Keep in mind:
+    //  For some reason, the color value is placed at the beginning
+    //  ... when a text shadow is retrieved via JavaScript...
+    //  [question]: Why?
+    // [ [text-shadow-string1-constituents], [text-shadow-string2-constituents] ]
+    // const currentTextShadowsConstituents = currentTextShadowsFromValue.map((value) => value.split(' '));
+    // for (const textShadowConsitutentArray of currentTextShadowsConstituents) {
+    //     const blurRadius = textShadowConsitutentArray.pop();
+    //     const textShadowYOffset = textShadowConsitutentArray.pop();
+    //     const textShadowXOffset = textShadowConsitutentArray.pop();
+        
+    //     const color = textShadowConsitutentArray.join('');
+    // }
+    // ---------------------
+    
+    // Now that we have our info from the current glow animation state,
+    // .. we may safely remove it and start the dynamic revert animation.
+    
+    
+    // We need not pause animations which also act upon the textShadow,
+    // for we specify that our animation is to replace other animations which act upon the same value.
+    // If they are played via CSS or if the other animations are played via JS and have a composite setting
+    // .. that doesn't replace, ours will win.
+    
+    // consideration:how to retain the state of the old animation such that there is no possibility of
+    // rendering a frame where the original animation has stopped and reverts back to the original style..?
+    
+    // ----------------------
+    // May become relevant if this library becomes heavily used and must become
+    // more generally suited: the options parameter w/ the subtree property.
+    // const currentAnimations = element.getAnimations();
+    // currentAnimations.forEach(animation => {
+    //     const keyframeEffect = animation.effect;
+       
+        // detect whether or not the animation effect contains a textShadow property key in its keyframes
+        // stop it if it does (or remove it somehow to allow the other properties to animate?)
+        // proceed with our own animation
+        
+        // Maybe I can make my own animation override the existing ones? somehow within the options of
+        // .. the Element.animate() method?
+        // the `composite` option for .animate() does default to replace. Does this apply to non-JS animations played via CSS?
+        // Answer: Yes.
+        
+        // Under what conditions will Animation.effect be null?
+    //     if (keyframeEffect === null) return;
+    // });
+    // currentAnimations.map((animation) => animation.stop());
+    // ----------------------
+    
+    /*
+        Fulfills the format consumed by Element.animate():
+            https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API/Keyframe_Formats
+    */
+    const keyframeObject = [
+        {
+            // A fallback for older browsers which don't support color deconstruction:
+            textShadow: "0 0 0 var(--color-white);",
+            // --color-off-white2 points to a deconstructed and darkend version of --color-white:
+            // textShadow: `${textShadowXOffset} ${textShadowYOffset} 0 var(--color-off-white2)`,
+            textShadow: currentTextShadowValue,
         },
         
         { textShadow: "0 0 0 transparent" },
@@ -75,14 +177,14 @@ function revertAnimation() {
 }
 
 anchorElement.addEventListener('mouseenter', () => {
-    anchorElement.classList.add('hover:glow');
-});
+}, { once: true });
 
+// how to handle animations stepping overriding one another's properties?
 anchorElement.addEventListener('mouseleave', () => {
-    const animation = revertAnimation();
+    debugger;
+    const animation = hideAnimatedTextShadows(anchorElement);
     
     animation.addEventListener('animationend', () => {
-        
     });
     // wait for animation to complete
     // add the hover listener back again
